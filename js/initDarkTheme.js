cbd4d58d3d6342ce294a0576f5eeb41ffeceb8cf
Leeloo Korben <37147870+anthonysalamin@users.noreply.github.com>
2026-03-23 17:22:52 +0800

    gradeint top bottom

--- js/initDarkTheme.js
+++ js/initDarkTheme.js
@@ -8,34 +8,40 @@ export function initDarkTheme() {
     const OPTIONS = {
       timeline: document.querySelector('[data-section="timeline"]'),
       partnerships: document.querySelector('[data-section="partnerships"]'),
-      overlays: document.querySelectorAll('[data-markee="overlay"]'),
+      overlaysUp: document.querySelectorAll('[data-markee="overlay-up"]'),
+      overlaysDown: document.querySelectorAll('[data-markee="overlay-down"]'),
       gradients: {
-        dark: `
-          linear-gradient(to bottom, hsla(0, 0%, 0%, 0) 70%, var(--black)),
-          linear-gradient(to bottom, var(--black), hsla(0, 0%, 0%, 0) 30%)
-        `,
-        light: `
-          linear-gradient(to bottom, hsla(0, 0%, 100%, 0) 70%, white),
-          linear-gradient(to bottom, white, hsla(0, 0%, 100%, 0) 30%)
-        `
+        up: {
+          dark: `linear-gradient(to bottom, var(--black), hsla(0, 0%, 0%, 0) 30%)`,
+          light: `linear-gradient(to bottom, white, hsla(0, 0%, 100%, 0) 30%)`
+        },
+        down: {
+          dark: `linear-gradient(to top, var(--black), hsla(0, 0%, 0%, 0) 30%)`,
+          light: `linear-gradient(to top, white, hsla(0, 0%, 100%, 0) 30%)`
+        }
       }
     };
     if (!OPTIONS.timeline || !OPTIONS.partnerships) return;
-    const updateOverlay = (gradient) => {
-      OPTIONS.overlays.forEach((el) => {
-        el.style.backgroundImage = gradient;
+
+    const updateOverlays = (theme) => {
+      OPTIONS.overlaysUp.forEach((el) => {
+        el.style.backgroundImage = OPTIONS.gradients.up[theme];
+      });
+      OPTIONS.overlaysDown.forEach((el) => {
+        el.style.backgroundImage = OPTIONS.gradients.down[theme];
       });
     };
+
     ScrollTrigger.create({
       trigger: OPTIONS.partnerships,
       start: "top top",
       onEnter: () => {
         OPTIONS.partnerships.classList.add("is-dark");
-        updateOverlay(OPTIONS.gradients.dark);
+        updateOverlays("dark");
       },
       onLeaveBack: () => {
         OPTIONS.partnerships.classList.remove("is-dark");
-        updateOverlay(OPTIONS.gradients.light);
+        updateOverlays("light");
       }
     });
   }
